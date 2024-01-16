96ed37a4f89ab086b8f18985b13457f1583c47c2
AlceaXich <shirmoc.t05@gmail.com>
2024-01-16 16:09:50 -0500

    Getting started with API routes

--- Node.js website/server.js
+++ Node.js website/server.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const req = require('express/lib/request');
 const app = express();
+//Display all fruits
+app.get('/fruits', (req,res) =>{
+    res.json({ msg: 'showing all red fruits'});
+});
+//Display fruits by id
+app.get('/fruits/:fruitsId', (req,res) => {
+    res.json({ msg: 'fruit id is ' + req.params.fruitsId });
+});
+//Create new fruit
+app.post('/fruits', (req,res) => {
+    res.json({ msg: 'Creating fruits with post' });
+});
 
+//Edit fruit with id
+app.put('/fruits/:fruitsId', (req,res) => {
+    res.json({ msg: 'Editing fruits ' + req.params.fruitsId });
+});
+
+//Delete fruits by id
+app.delete('/fruits/:fruitsId', (req,res) => {
+    res.json({ msg: 'Deleting fruits ' + req.params.fruitsId });
+});
+
+//Welcome to index
 app.get('/',(req, res) => {
     res.json({msg: 'API Server is running',version: 1});
 });

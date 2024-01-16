faae02e4b851723e54cfda9b4f7de6f16e8eeffd
AlceaXich <shirmoc.t05@gmail.com>
2024-01-16 15:41:10 -0500

    Example of data json using POSTMAN

--- Node.js website/server.js
+++ Node.js website/server.js
@@ -3,7 +3,7 @@ const req = require('express/lib/request');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send('Hello worl, I am AlceaXich using nodemon :D');
+    res.json({msg: 'API Server is running',version: 1});
 });
 
 app.listen(3000, console.log('Server started on port 3000'));
